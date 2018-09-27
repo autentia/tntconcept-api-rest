d64b531467edc7cba8b71019686a98b6fb63a3eb
mahernandez <mahernandez@autentia.com>
2018-09-27 08:46:19 +0200

    App as spring boot war

--- src/main/java/com/autentia/tnt/api/rest/ApiRestTntApplication.java
+++ src/main/java/com/autentia/tnt/api/rest/ApiRestTntApplication.java
@@ -19,11 +19,12 @@ package com.autentia.tnt.api.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class ApiRestTntApplication {
+public class ApiRestTntApplication extends SpringBootServletInitializer{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestTntApplication.class, args);
